1e50ee0a21e2ab8824ce48e41bf6e51ec14d4cc6
kilker12 <joey@mjhosting.net>
2013-09-05 12:36:28 -0400

    Update GPIO.cpp

--- libraries/AP_HAL_Empty/GPIO.cpp
+++ libraries/AP_HAL_Empty/GPIO.cpp
@@ -1,50 +1,49 @@
-
 #include "GPIO.h"
 
-using namespace Empty;
+using namespace AIOPV2;
 
-EmptyGPIO::EmptyGPIO()
+AIOPV2GPIO::AIOPV2GPIO()
 {}
 
-void EmptyGPIO::init()
+void AIOPV2GPIO::init()
 {}
 
-void EmptyGPIO::pinMode(uint8_t pin, uint8_t output)
+void AIOPV2GPIO::pinMode(uint8_t pin, uint8_t output)
 {}
 
-int8_t EmptyGPIO::analogPinToDigitalPin(uint8_t pin)
+int8_t AIOPV2GPIO::analogPinToDigitalPin(uint8_t pin)
 {
 	return -1;
 }
 
 
-uint8_t EmptyGPIO::read(uint8_t pin) {
+uint8_t AIOPV2GPIO::read(uint8_t pin) {
     return 0;
 }
 
-void EmptyGPIO::write(uint8_t pin, uint8_t value)
+void AIOPV2GPIO::write(uint8_t pin, uint8_t value)
 {}
 
 /* Alternative interface: */
-AP_HAL::DigitalSource* EmptyGPIO::channel(uint16_t n) {
-    return new EmptyDigitalSource(0);
+AP_HAL::DigitalSource* AIOPV2GPIO::channel(uint16_t n) {
+    return new AIOPV2DigitalSource(0);
 }
 
 /* Interrupt interface: */
-bool EmptyGPIO::attach_interrupt(uint8_t interrupt_num, AP_HAL::Proc p,
+bool AIOPV2GPIO::attach_interrupt(uint8_t interrupt_num, AP_HAL::Proc p,
         uint8_t mode) {
     return true;
 }
 
 
-EmptyDigitalSource::EmptyDigitalSource(uint8_t v) :
+AIOPV2DigitalSource::AIOPV2DigitalSource(uint8_t v) :
     _v(v)
 {}
 
-void EmptyDigitalSource::mode(uint8_t output)
+void AIOPV2DigitalSource::mode(uint8_t output)
 {}
 
-uint8_t EmptyDigitalSource::read() {
+uint8_t AIOPV2DigitalSource::read() {
     return _v;
 }
 
