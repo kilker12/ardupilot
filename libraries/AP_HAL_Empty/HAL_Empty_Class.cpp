6a0ea0d1625321ae81a5988e64a52e8de19456e8
kilker12 <joey@mjhosting.net>
2013-09-05 12:39:58 -0400

    Update HAL_Empty_Class.cpp

--- libraries/AP_HAL_Empty/HAL_Empty_Class.cpp
+++ libraries/AP_HAL_Empty/HAL_Empty_Class.cpp
@@ -1,26 +1,25 @@
-
 #include <AP_HAL.h>
-#if CONFIG_HAL_BOARD == HAL_BOARD_EMPTY
+#if CONFIG_HAL_BOARD == HAL_BOARD_AIOPV2
 
-#include "HAL_Empty_Class.h"
-#include "AP_HAL_Empty_Private.h"
+#include "HAL_AIOPV2_Class.h"
+#include "AP_HAL_AIOPV2_Private.h"
 
-using namespace Empty;
+using namespace AIOPV2;
 
-static EmptyUARTDriver uartADriver;
-static EmptyUARTDriver uartBDriver;
-static EmptyUARTDriver uartCDriver;
-static EmptySemaphore  i2cSemaphore;
-static EmptyI2CDriver  i2cDriver(&i2cSemaphore);
-static EmptySPIDeviceManager spiDeviceManager;
-static EmptyAnalogIn analogIn;
-static EmptyStorage storageDriver;
-static EmptyConsoleDriver consoleDriver(&uartADriver);
-static EmptyGPIO gpioDriver;
-static EmptyRCInput rcinDriver;
-static EmptyRCOutput rcoutDriver;
-static EmptyScheduler schedulerInstance;
-static EmptyUtil utilInstance;
+static AIOPV2UARTDriver uartADriver;
+static AIOPV2UARTDriver uartBDriver;
+static AIOPV2UARTDriver uartCDriver;
+static AIOPV2Semaphore  i2cSemaphore;
+static AIOPV2I2CDriver  i2cDriver(&i2cSemaphore);
+static AIOPV2SPIDeviceManager spiDeviceManager;
+static AIOPV2AnalogIn analogIn;
+static AIOPV2Storage storageDriver;
+static AIOPV2ConsoleDriver consoleDriver(&uartADriver);
+static AIOPV2GPIO gpioDriver;
+static AIOPV2RCInput rcinDriver;
+static AIOPV2RCOutput rcoutDriver;
+static AIOPV2Scheduler schedulerInstance;
+static AIOPV2Util utilInstance;
 
 HAL_Empty::HAL_Empty() :
     AP_HAL::HAL(
